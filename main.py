bfa9f1340aa439c1850ba8745f669df8201e7524
DWAA1660 <dwatnip123@gmail.com>
2023-11-02 21:08:43 -0600

    yes

---
+++ main.py
@@ -0,0 +1,93 @@
+from flask import Flask
+import sqlite3
+import yt_dlp as youtube_dl
+import requests
+import os
+from apscheduler.schedulers.background import BackgroundScheduler
+from flask import Flask, request
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Integer, String, text
+from sqlalchemy.orm import Mapped, mapped_column
+import shutil
+import time
+import threading
+
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db = SQLAlchemy(app)
+# db.init_app(app)
+
+class Song(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255), nullable=False)
+    artist = db.Column(db.String(255), nullable=False)
+
+
+with app.app_context():
+    db.create_all()
+scheduler = BackgroundScheduler()
+scheduler.start()
+
+
+
+def sync_db():
+    with app.app_context():
+        res = db.session.execute(text("SELECT * FROM song"))
+        print(res.fetchall())
+        for file in os.listdir("static/downloaded"):
+            info = file.split("@")
+            if file.endswith(".mp3") and file.replace(".mp3", ".webm") not in os.listdir("static/downloaded"):
+                if file not in os.listdir("static/indb"):
+                    song = Song(title=info[1], artist=info[0])
+                    db.session.add(song)
+                    db.session.commit()
+                    shutil.move(f"static/downloaded/{file}", f"static/indb")
+                else:
+                    os.remove(f"static/downloaded/{file}")
+
+scheduler.add_job(sync_db, 'interval', seconds=5)
+
+    
+
+def downloadsong(url: str):
+    try:
+        ydl_opts = {
+            'format': 'bestaudio/best',
+            'postprocessors': [{
+                'key': 'FFmpegExtractAudio',
+                'preferredcodec': 'mp3',
+                'preferredquality': '192',
+            }],
+            'verbose': True,
+            'outtmpl': 'static/downloaded/%(artist)s@%(title)s.%(ext)s',
+        }
+
+        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            ydl.download(url)
+            
+    except Exception:
+        pass
+
+# downloadsong("https://music.youtube.com/playlist?list=PLiEWsl0vGZ6DUba7stAZlQt2pU5EZUXQl&si=n15MurPxImfz-ejL")
+
+
+@app.route("/download", methods=["POST"])
+def index():
+    url = request.headers.get("url")
+    threading.Thread(target=downloadsong, args=(url,)).start()
+    return "Hello, World!"
+
+
+# @app.route("/test")
+# async def test():
+#     return "Test"
+
+
+# @app.route("/test2")
+# async def test2():
+#     return "Test2"
+
+if __name__ == "__main__":
+    app.run(debug=False)
